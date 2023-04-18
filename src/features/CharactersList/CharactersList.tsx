733fb644f9713906fa762bb2bfd392726c272ed9
Ekaterina <vam.pochta@yahoo.com>
2023-04-18 22:23:54 +0400

    changes card into list

--- src/features/CharactersList/CharactersList.tsx
+++ src/features/CharactersList/CharactersList.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 
-import { Pagination } from 'antd';
+import { Pagination, List } from 'antd';
 
 import Card from 'entities/Card/Card';
 import {ICardProps} from 'entities/Card/Card';
@@ -32,14 +32,25 @@ const CharactersList = () => {
 	}, []);
 
   return (
-		<div className={styles.CharactersList}>
-			{charactersData?.map((item: ICardProps) => (
+		
+
+		<>	
+		{/* <div className={styles.CharactersList}> */}
+			{/* {charactersData?.map((item: ICardProps) => (
 			<div key={item?.name} className={styles.CharactersList__Card}>
 				<Card name={item?.name} height={item.height} mass={item.mass} birth_year={item.birth_year} />
-			</div>))}
+			</div>))} */}
+			<List
+				size="small"
+				// header={<div>Header</div>}
+				// footer={<div>Footer</div>}
+				bordered
+				dataSource={charactersData}
+				renderItem={(item: ICardProps) => <List.Item>{item.name}</List.Item>}
+			/>
 
 			<Pagination defaultCurrent={page} total={total} />;
-		</div>
+		</>
 	)
 }
 
